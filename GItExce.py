1d1b74a456931198cd6daec6e40164962b05b878
Hassan-Brar <hassanbrar@gmail.com>
2021-09-17 12:23:32 -0700

    changes

--- GItExce.py
+++ GItExce.py
@@ -6,3 +6,14 @@ print(num2)
 equals=int(num1)+int(num2)
 
 print("Your numbers added together equal to", equals)
+
+
+if(int(num1) % 2 == 0):                        #Checking if numbers are even or odd
+    print(num1 + 'your first number is an even number!')
+else:
+    print(num1 + 'your first number is an odd number!')
+
+if(int(num2) % 2 == 0):                        
+    print(num2 + 'your second number is an even number!')
+else:
+    print(num2 + 'your second number is an odd number!')
