f43ad114430aace4f330d0925df38cb7a5caa07b
Hassan-Brar <hassanbrar@gmail.com>
2021-09-17 12:17:53 -0700

    init

---
+++ GItExce.py
@@ -0,0 +1,8 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+print(num1)
+print(num2)
+
+equals=int(num1)+int(num2)
+
+print("Your numbers added together equal to", equals)
